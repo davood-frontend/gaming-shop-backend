d3d2c3470ea08b495e9ab3c49b05c06aae15002e
David <davood.frontend@gmail.com>
2024-05-11 11:17:56 -0700

    added all routes

---
+++ app/api/consoles/route.js
@@ -0,0 +1,12 @@
+import connect from "@/app/utils/db";
+import { NextResponse } from "next/server";
+import Console from "@/app/models/Console";
+export const GET = async () => {
+    try {
+        await connect();
+        const res = await Console.find()
+        return new NextResponse(JSON.stringify(res), { status: 200 })
+    } catch (err) {
+        return new NextResponse(err, { status: 500 })
+    }
+}
